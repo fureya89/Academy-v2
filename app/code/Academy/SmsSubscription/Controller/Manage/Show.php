a3bfe41f87daca1f06f84b0b34f6a3cfa9a7ebd1
Ewelina Zuchowska <ewelinagoszczurna@gmail.com>
2021-09-23 15:19:10 +0200

    TASK-4. Fixed Dependency Injection in SMS Subscription module

--- app/code/Academy/SmsSubscription/Controller/Manage/Show.php
+++ app/code/Academy/SmsSubscription/Controller/Manage/Show.php
@@ -16,21 +16,16 @@ class Show extends \Magento\Framework\App\Action\Action implements ActionInterfa
     public function __construct(
         Context $context,
         \Magento\Framework\View\Result\PageFactory $pageFactory,
-        \Academy\SmsSubscription\Model\ResourceModel\SmsSubscription\Collection $collectionFactory)
+        \Academy\SmsSubscription\Model\ResourceModel\SmsSubscription\CollectionFactory $collectionFactory)
     {
         $this->_pageFactory = $pageFactory;
         $this->_collection= $collectionFactory;
         parent::__construct($context);
     }
 
-    public function execute(): \Magento\Framework\Controller\ResultInterface
+    public function execute()//: \Magento\Framework\Controller\ResultInterface
     {
-        $smsSubscriptionCollection = $this->_collection->create();
-        echo "<pre>";
-        foreach ($smsSubscriptionCollection as $smsSubscription){
-            print_r($smsSubscription->getData());
-            print_r('test');
-        }
+        //$smsSubscriptionCollection = $this->_collection->create();
 
         return $this->_pageFactory->create();
     }
